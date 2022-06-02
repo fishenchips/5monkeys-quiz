09ca20513fa047873d4e394007d5e11e45c4f872
Philip Carlsson <philipcarlsson@hotmail.com>
2022-06-02 10:19:03 +0200

    adding delivery options code and styling

--- src/UI/cart/Cart.js
+++ src/UI/cart/Cart.js
@@ -1,10 +1,11 @@
 import React from "react";
 import CartDelivery from "../../components/cartItems/CartDelivery";
 import CartItems from "../../components/cartItems/CartItems";
+import styles from "./Cart.module.css";
 
 function Cart() {
   return (
-    <div>
+    <div className={styles.cart}>
       <CartItems />
       <CartDelivery />
     </div>
--- src/components/cartItems/CartDelivery.js
+++ src/components/cartItems/CartDelivery.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTruck, faHotel } from "@fortawesome/free-solid-svg-icons";
+import styles from "./CartDelivery.module.css";
 
 function CartDelivery() {
+  let deliveryFee = 79;
+
   return (
-    <div>
-      CartDelivery
-      <div>
+    <div className={styles.deliveryDiv}>
+      <div className={styles.deliveryChoice}>
         <FontAwesomeIcon icon={faTruck} flip="horizontal" />
+        <input type="radio" name="delivery" id="home" value="home" />
+        <img />
+        <label htmlFor="home">Hemleverans</label>
+        <span className={styles.deliveryFee}>{deliveryFee} kr</span>
+      </div>
+      <div className={styles.deliveryBreak}></div>
+      <div className={styles.deliveryChoice}>
         <FontAwesomeIcon icon={faHotel} />
+        <input type="radio" name="delivery" id="store" value="store" />
+        <img />
+        <label htmlFor="store">Hämta i butik</label>
       </div>
     </div>
   );
