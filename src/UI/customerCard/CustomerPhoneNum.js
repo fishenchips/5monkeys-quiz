486bcaf965970662cb6d4e197e9c0c11a29bf444
Philip Carlsson <philipcarlsson@hotmail.com>
2022-06-04 22:04:03 +0200

    adding state and onChange functions to the input tags

--- src/UI/customerCard/CustomerPhoneNum.js
+++ src/UI/customerCard/CustomerPhoneNum.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import styles from "./CustomerPhoneNum.module.css";
 
 function CustomerPhoneNum() {
+  const [phoneNum, setPhoneNum] = useState("070125678");
+
+  const handlePhoneNumber = (e) => {
+    setPhoneNum(e.target.value);
+  };
+
   return (
     <div className={styles.phoneDiv}>
       <div className={styles.innerPhoneDiv}>
         <div className={styles.phoneForm}>
           <label className={styles.phoneLabel}>MOBILNUMMER:</label>
           <form>
-            <input type="number" maxLength="9" min="0" max="9" />
+            <input
+              type="text"
+              maxLength="9"
+              value={phoneNum}
+              onChange={handlePhoneNumber}
+            />
           </form>
         </div>
         <div className={styles.changePhone}>
--- src/UI/customerCard/SSNCard.js
+++ src/UI/customerCard/SSNCard.js
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card";
 import styles from "./SSNCard.module.css";
 
 function SSNCard() {
+  const [birthDate, setBirthDate] = useState("720324");
+  const [lastFour, setLastFour] = useState("1083");
+
+  const handleBirthDate = (e) => {
+    //only accept numbers and backspace
+    const re = /^[0-9\b]+$/;
+
+    // if value is not blank, then test the regex
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setBirthDate({ value: e.target.value });
+    }
+
+    setBirthDate(e.target.value);
+
+    //if user has entered 6 digits, move to the next input
+    if (e.target.value.length === 6) {
+      const form = e.target.form;
+      const index = [...form].indexOf(e.target);
+
+      form.elements[index + 1].focus();
+    }
+  };
+
+  const handleLastFour = (e) => {
+    const re = /^[0-9\b]+$/;
+
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setLastFour({ value: e.target.value });
+    }
+
+    setLastFour(e.target.value);
+  };
+
   return (
     <Card>
       <label className={styles.label}>PERSONNUMMER:</label>
       <form className={styles.ssnForm}>
-        <input type="number" min="0" max="9" maxLength="6"></input>
+        <input
+          className={styles.birthDateInput}
+          type="text"
+          maxLength="6"
+          value={birthDate}
+          onChange={handleBirthDate}
+        ></input>
         <h2>-</h2>
-        <input type="number" min="0" max="9" maxLength="4"></input>
+        <input
+          className={styles.lastFourInput}
+          type="text"
+          maxLength="4"
+          value={lastFour}
+          onChange={handleLastFour}
+        ></input>
       </form>
     </Card>
   );
